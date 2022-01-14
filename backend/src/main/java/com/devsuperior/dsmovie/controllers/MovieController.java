91ec755d0dc4b8dabf4416ddd9f18f209c235340
eevaristo <elton.evaristo@gmail.com>
2022-01-14 14:11:00 -0300

    First request

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.services.MovieService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,17 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
-    
-    @Autowired
-    private MovieService movieService;
 
-    @GetMapping
-    public Page<MovieDTO> findAll(Pageable pageable) {
-        return movieService.findAll(pageable);
-    }
+  @Autowired
+  private MovieService movieService;
 
-    @GetMapping(value = "/{id}")
-    public MovieDTO findById(@PathVariable Long id) {
-        return movieService.findById(id);
-    }
+  @GetMapping
+  public Page<MovieDTO> findAll(Pageable pageable) {
+    return movieService.findAll(pageable);
+  }
+
+  @GetMapping(value = "/{id}")
+  public MovieDTO findById(@PathVariable Long id) {
+    return movieService.findById(id);
+  }
 }
--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmovie.entities;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,71 +13,75 @@ import javax.persistence.Table;
 @Table(name = "tb_movie")
 public class Movie {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private Double score;
-    private Integer count;
-    private String image;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @OneToMany(mappedBy = "id.movie")
-    private Set<Score> scores = new HashSet<>();
+  private String title;
+  private Double score;
+  private Integer count;
+  private String image;
 
-    public Movie() {
+  @OneToMany(mappedBy = "id.movie")
+  private Set<Score> scores = new HashSet<>();
 
-    }
+  public Movie() {}
 
-    public Movie(Long id, String title, Double score, Integer count, String image) {
-        this.id = id;
-        this.title = title;
-        this.score = score;
-        this.count = count;
-        this.image = image;
-    }
+  public Movie(
+    Long id,
+    String title,
+    Double score,
+    Integer count,
+    String image
+  ) {
+    this.id = id;
+    this.title = title;
+    this.score = score;
+    this.count = count;
+    this.image = image;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public Double getScore() {
-        return score;
-    }
+  public Double getScore() {
+    return score;
+  }
 
-    public void setScore(Double score) {
-        this.score = score;
-    }
+  public void setScore(Double score) {
+    this.score = score;
+  }
 
-    public Integer getCount() {
-        return count;
-    }
+  public Integer getCount() {
+    return count;
+  }
 
-    public void setCount(Integer count) {
-        this.count = count;
-    }
+  public void setCount(Integer count) {
+    this.count = count;
+  }
 
-    public String getImage() {
-        return image;
-    }
+  public String getImage() {
+    return image;
+  }
 
-    public void setImage(String image) {
-        this.image = image;
-    }
+  public void setImage(String image) {
+    this.image = image;
+  }
 
-    public Set<Score> getScores() {
-        return scores;
-    }
-
+  public Set<Score> getScores() {
+    return scores;
+  }
 }
